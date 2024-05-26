a8ef3df3ffebe71ca637d9bce22b307c20f6a22e
Leonardo Mejía <leomejia646@gmail.com>
2024-05-26 10:01:44 -0600

    Codelab: Persistencia en memoria completo

---
+++ src/main/java/com/example/h2_demo/controller/CountryController.java
@@ -0,0 +1,23 @@
+package com.example.h2_demo.controller;
+
+import com.example.h2_demo.model.Country;
+import com.example.h2_demo.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/countries")
+public class CountryController {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @GetMapping
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+}
